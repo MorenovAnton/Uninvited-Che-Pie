72cde07e21a245b140e4e3798d9a3802395de156
Anton <43759219+MorenovAnton@users.noreply.github.com>
2021-02-16 18:12:03 +0300

    Update connection_parameters.py

--- connection_parameters.py
+++ connection_parameters.py
@@ -1,12 +1,12 @@
 
 def return_token():
-    return '1288091950:AAGtzfTqchhqiIWbu8jxOUJBWBDaqJ-5Q4I'
+    return '..........:...........................-....'
 
 def mysql_parameters():
-    user = 'bcdef8816f352f'
-    daatabase = 'heroku_17d32a8c1f17b74'
-    password = '22a0cb81'
-    mysql_server  = 'us-cdbr-east-02.cleardb.com'
+    user = '........'
+    daatabase = 'heroku_............'
+    password = '..........'
+    mysql_server  = '..-....-....-.........com'
     return mysql_server, user, password, daatabase
 
 
